bc678ba080672e4f76a374a9856abd348fd22656
mhhasanmehedi <mmmehedi53@gmail.com>
2023-08-24 18:05:31 +0600

    f

---
+++ main.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", function () {
+  // Hide the loading spinner when the page is fully loaded
+  var spinnerContainer = document.querySelector("#spinner");
+  spinnerContainer.style.display = "none";
+});
